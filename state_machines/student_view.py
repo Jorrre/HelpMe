72df3d9b593e537cdcdc7c6f99cd1a812f1dd5f6
Jørgen Reimers <jorgenreim@gmail.com>
2023-03-27 15:44:28 +0200

    Think stopwatches and progress work
    Haven't tried

--- state_machines/student_view.py
+++ state_machines/student_view.py
@@ -1,7 +1,12 @@
 from stmpy import Machine, Driver
+import time
+
 class StudentView:
-  def __init__(self):
-    self.stm = None
+  def __init__(self, max_prorgess=5):
+    self.progress = 0
+    self.max_prorgess = max_prorgess
+    self.wait_for_help_stopwatch_start_time = 0
+    self.help_stopwatch_start_time = 0
 
   def display_group_number(self):
     print("Displaying group number")
@@ -14,15 +19,41 @@ class StudentView:
 
   def increase_progress(self):
     print("Increasing progress")
+    if (self.progress < self.max_prorgess):
+      self.progress += 1
+      print("Current progress: " + self.progress)
 
   def decrease_progress(self):
     print("Decreasing progress")
+    if (self.progress > 0):
+      self.progress -= 1
+      print("Current progress: " + self.progress)
 
-  def start_stopwatch(self, t):
-    print("Starting timer: " + t)
+  def start_wait_for_help_stopwatch(self):
+    print("Starting wait for help stopwatch")
+    assert(self.wait_for_help_stopwatch_start_time == 0) # Make sure the stopwatch isn't already running
+    self.wait_for_help_stopwatch_start_time = time.time()
 
-  def stop_stopwatch(self, t):
-    print("Starting timer: " + t)
+  def stop_wait_for_help_stopwatch(self):
+    print("Stopping wait for help stopwatch: " + t)
+    elapsed_time = time.time() - self.wait_for_help_stopwatch_start_time
+    print(f"Wait for help stopwatch used {elapsed_time} seconds")
+    self.wait_for_help_stopwatch_start_time = 0 # restart stopwatch
+
+  def cancel_wait_for_help_stopwatch(self):
+    print("Cancelling waiting for help stopwatch")
+    self.wait_for_help_stopwatch_start_time = 0 # restart stopwatch
+
+  def start_help_stopwatch(self):
+    print("Starting wait for help stopwatch")
+    assert(self.help_stopwatch_start_time == 0) # Make sure the stopwatch isn't already running
+    self.help_stopwatch_start_time = time.time()
+
+  def stop_help_stopwatch(self):
+    print("Stopping help stopwatch: " + t)
+    elapsed_time = time.time() - self.help_stopwatch_start_time
+    print(f"Help stopwatch used {elapsed_time} seconds")
+    self.help_stopwatch_start_time = 0 # restart stopwatch
 
 student_view = StudentView()
 
@@ -41,13 +72,15 @@ t_press_help = {
 t_cancel_help = {
   "trigger": "cancel_help",
   "source": "need_help",
-  "target": "no_help"
+  "target": "no_help",
+  "effect": "cancel_wait_for_help_stopwatch()"
 }
 
 t_TA_accepts_to_help = {
   "trigger": "TA_accepts_to_help",
   "source": "need_help",
-  "target": "receiving_help"
+  "target": "receiving_help",
+  "effect": "stop_wait_for_help_stopwatch()",
 }
 
 t_TA_finished_help = {
@@ -66,8 +99,7 @@ s_no_help = {
 s_need_help = {
   "name": "need_help",
   "entry": "light_on()",
-  "entry": "start_stopwatch()",
-  "exit": "stop_stopwatch()",
+  "entry": "start_wait_for_help_stopwatch()",
   "press_forwards": "increase_progress()",
   "press_backwards": "decrease_progress()"
 }
@@ -87,8 +119,6 @@ student_view_machine = Machine(
   transitions=[t_init, t_press_help, t_cancel_help, t_TA_accepts_to_help, t_TA_finished_help], 
   states=[s_no_help, s_need_help, s_receiving_help]  
 )
-
-student_view.stm = student_view_machine
 
 driver = Driver()
 driver.add_machine(student_view_machine)
