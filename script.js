b01945ae536dc623b7bac5985e12e9894ac9a9e3
emlie <elisenbergm@gmail.com>
2023-03-27 16:23:46 +0200

    setup for js

---
+++ script.js
@@ -0,0 +1,22 @@
+// GET HTML ELEMENTS
+const containerGroups = document.querySelector('#col-2-container-groups');
+
+
+
+
+// LOAD GROUPS AND PROGRESS BARS
+
+
+
+
+// BUTTON REGRESS
+
+
+
+
+// BUTTON PROGRESS
+
+
+
+
+// BUTTON HELP
