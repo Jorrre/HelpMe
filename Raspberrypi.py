3f929b35a9ee015c07ac954bc60defef264ab0ed
Vuuuuu <johan.2000@hotmail.no>
2023-04-17 17:27:21 +0200

    mqtt added on_message function to recieve messages

--- Raspberrypi.py
+++ Raspberrypi.py
@@ -12,6 +12,7 @@ red = (255, 0, 0)
 difficulty = [green, orange, red]
 
 unit_5 = ["Q1", "Q2", "Q3", "Q4"]
+
 unit = "unit6"
 group = "G6"
 status = 0
@@ -26,13 +27,12 @@ while True:
 
             if event.direction == "left" and status > 1:
                 status = status - 1
+                myclient.send_status(unit, group, unit_5[status])
             elif event.direction == "right" and status < len(unit_5) - 2:
                 status = status + 1
-
-            if event.direction == "middle":
+                myclient.send_status(unit, group, unit_5[status])
+            elif event.direction == "middle":
                 myclient.send_status(unit, group, "Help")
-
-            myclient.send_status(unit, group, unit_5[status])
 
     sense.show_message(group + ":" + unit_5[status], text_colour=white)
     sense.clear()
