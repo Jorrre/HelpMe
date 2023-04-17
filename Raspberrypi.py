9a09f39b0d45d182f92cda2d7bdc5ad01985aefc
Vuuuuu <johan.2000@hotmail.no>
2023-04-17 16:54:33 +0200

    mqtt test 3

--- Raspberrypi.py
+++ Raspberrypi.py
@@ -11,18 +11,17 @@ orange = (255, 165, 0)
 red = (255, 0, 0)
 difficulty = [green, orange, red]
 
-unit_5 = ["Help", "Q1", "Q2", "Q3", "Q4"]
-
+unit_5 = ["Q1", "Q2", "Q3", "Q4"]
 unit = "unit6"
 group = "G6"
-
-status = 1
-difficulty = [green, orange, red]
+status = 0
 
 myclient = MQTT_Client()
 myclient.start(broker, port)
 
 while True:
+    myclient.send_status(unit, group, unit_5[status])
+
     for event in sense.stick.get_events():
         if event.action == "pressed":
 
@@ -31,15 +30,10 @@ while True:
             elif event.direction == "right" and status < len(unit_5) - 2:
                 status = status + 1
 
-            if status > len(unit_5) - 1:
-                status = 1
-            elif status < 1:
-                status = len(unit_5) - 1
-
             if event.direction == "middle":
-                sense.show_message("REQ SOS", text_colour=white)
+                myclient.send_status(unit, group, "Help")
 
             myclient.send_status(unit, group, unit_5[status])
 
-    sense.show_message(group + ":" + unit_5[status])
+    sense.show_message(group + ":" + unit_5[status], test_colour=white)
     sense.clear()
