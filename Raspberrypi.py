06ae4b6c68d635917c19d4878bfb964aa6c4df76
Vuuuuu <johan.2000@hotmail.no>
2023-04-17 16:47:18 +0200

    mqtt test 3

--- Raspberrypi.py
+++ Raspberrypi.py
@@ -11,13 +11,12 @@ orange = (255, 165, 0)
 red = (255, 0, 0)
 difficulty = [green, orange, red]
 
-unit_5 = ["Q1", "Q2", "Q3", "Q4"]
-unit_5_diff = [0, 0, 0, 0]
+unit_5 = ["Help", "Q1", "Q2", "Q3", "Q4"]
 
 unit = "unit6"
-group = "group6"
+group = "G6"
 
-count = 0
+status = 1
 difficulty = [green, orange, red]
 
 myclient = MQTT_Client()
@@ -27,20 +26,20 @@ while True:
     for event in sense.stick.get_events():
         if event.action == "pressed":
 
-            if event.direction == "left":
-                count = count - 1
-            elif event.direction == "right":
-                count = count + 1
+            if event.direction == "left" and status > 1:
+                status = status - 1
+            elif event.direction == "right" and status < len(unit_5) - 2:
+                status = status + 1
 
-            if count > len(unit_5) - 1:
-                count = 0
-            elif count < 0:
-                count = len(unit_5) - 1
+            if status > len(unit_5) - 1:
+                status = 1
+            elif status < 1:
+                status = len(unit_5) - 1
 
             if event.direction == "middle":
-                sense.show_message("REQ SOS ", text_colour=white)
+                sense.show_message("REQ SOS", text_colour=white)
 
-            myclient.send_status(unit, group, count)
+            myclient.send_status(unit, group, unit_5[status])
 
-    sense.show_message(group + ":" + unit_5[count], text_colour=difficulty[unit_5_diff[count]])
+    sense.show_message(group + ":" + unit_5[status])
     sense.clear()
