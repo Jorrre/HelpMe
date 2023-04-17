40530c3fd7bd6afbc77ed1d34947731757eadee2
Vuuuuu <43222738+Vuuuuu@users.noreply.github.com>
2023-04-17 17:39:14 +0200

    RPI Client code
    * first code test joystick
    
    * first code test joystick
    
    * second version of sensehat code
    
    * mqtt
    
    * mqtt test 2
    
    * mqtt test 3
    
    * mqtt test 3
    
    * mqtt fixed test_colour -> text_colour bug
    
    * mqtt fixed loop bug
    
    * mqtt added on_message function to recieve messages
    
    * added self.on_message in start
    
    * added unit6

--- Raspberrypi.py
+++ Raspberrypi.py
@@ -1,1 +1,38 @@
-## test
+from sense_hat import SenseHat
+from time import sleep
+from Raspberrypi_client import MQTT_Client
+
+broker, port = "mqtt20.item.ntnu.no", 1883
+
+sense = SenseHat()
+white = (255, 255, 255)
+green = (0, 255, 0)
+orange = (255, 165, 0)
+red = (255, 0, 0)
+difficulty = [green, orange, red]
+
+unit_5 = ["Q1", "Q2", "Q3", "Q4"]
+
+unit = "unit6"
+group = "G6"
+status = 0
+
+myclient = MQTT_Client()
+myclient.start(broker, port)
+myclient.send_status(unit, group, unit_5[status])
+
+while True:
+    for event in sense.stick.get_events():
+        if event.action == "pressed":
+
+            if event.direction == "left" and status > 1:
+                status = status - 1
+                myclient.send_status(unit, group, unit_5[status])
+            elif event.direction == "right" and status < len(unit_5) - 2:
+                status = status + 1
+                myclient.send_status(unit, group, unit_5[status])
+            elif event.direction == "middle":
+                myclient.send_status(unit, group, "Help")
+
+    sense.show_message(group + ":" + unit_5[status], text_colour=white)
+    sense.clear()
---
+++ Raspberrypi_client.py
@@ -0,0 +1,36 @@
+import paho.mqtt.client as mqtt
+from time import sleep
+from threading import Thread
+
+class MQTT_Client:
+    def on_connect(self, client, userdata, flags, rc):
+        print("on_connect(): {}".format(mqtt.connack_string(rc)))
+
+    def start(self, broker, port):
+        self.client = mqtt.Client()
+        self.client.on_message = self.on_message
+        self.client.on_connect = self.on_connect
+        print("Connecting to {}:{}".format(broker, port))
+        self.client.connect(broker, port)
+
+        self.client.subscribe("g6/unit6/G6")
+
+        try:
+            thread = Thread(target=self.client.loop_forever)
+            thread.start()
+        except KeyboardInterrupt:
+            print("Interrupted")
+            self.client.disconnect()
+
+    def on_message(self, client, userdata, msg):
+        try:
+            print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+        except e:
+            print(e)
+
+    def send_status(self, unit, group, status):
+        try:
+            self.client.publish("g6/" + unit + "/" + group, status)
+        except e:
+            print("error")
+
