1e789e5052f17e4528ffe46ff9dfa2a8a4dc68c0
emlie <elisenbergm@gmail.com>
2023-04-17 15:32:50 +0200

    change button look when asking for help

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const elContainerGroups = document.querySelector('#col-2-container-groups');
 const elMyGroupBarFiller = document.querySelector('#myGroup-bar-filler');
 const elBtnRegress = document.querySelector('#btn-regress');
 const elBtnHelp = document.querySelector('#btn-help');
+const elBtnHelpIcon = document.querySelector('#btn-help-icon');
 const elBtnProgress = document.querySelector('#btn-progress');
 
 // GET THE ROOT ELEMENT
@@ -15,6 +16,7 @@ const barStep = (100/numberOfTasks) + 5;
 
 // VARIABLES
 let myBarLength = 0;
+let groupStatus = 0; // 0 = working; 1 = need help
 
 // STARTUP
 r.style.setProperty('--bar-filler-width', `${myBarLength}%`);
@@ -90,8 +92,35 @@ function btnProgress(event) {
 
 
 // BUTTON HELP
+function btnHelp(event) {
+  event.preventDefault();
 
+  if (!groupStatus) {
+    // elBtnHelpIcon.className = 'fa-circle-notch fa-spin';
+    elBtnHelp.className = 'btn-help-asking';
 
+    elBtnHelp.innerHTML = `
+      <i id="btn-help-icon" class="fa-solid fa-circle-notch fa-spin" style="margin-bottom: 1rem; font-size: 3rem; color: var(--color-blue);"></i>
+      Awaiting Help
+    `;
+
+    groupStatus = 1;
+    console.log("HELP ME!");
+    console.log(elBtnHelpIcon);
+  } else {
+    // elBtnHelpIcon.className = 'fa-solid fa-circle-question';
+    elBtnHelp.className = 'btn-help-idle';
+    
+    elBtnHelp.innerHTML = `
+      <i id="btn-help-icon" class="fa-solid fa-circle-question" style="margin-bottom: 1rem; font-size: 3rem; color: var(--color-white);"></i>
+      Ask for Help
+    `;
+
+    groupStatus = 0;
+    console.log("back to work");
+    console.log(elBtnHelpIcon);
+  }
+}
 
 
 // RUN
