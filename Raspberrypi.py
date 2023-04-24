a974c09284bb48a3b003c12ff6f21c447cf8c792
Vuuuuu <johan.2000@hotmail.no>
2023-04-24 16:04:02 +0200

    casting error

--- Raspberrypi.py
+++ Raspberrypi.py
@@ -25,7 +25,8 @@ def handleMessage(msg):
     print(msg)
 
     if msg[0] == "update_progress":
-        status = msg[1]
+        print(type(msg[1]))
+        status = int(msg[1])
     if msg[0] == "help_off":
         helpFlag = False
     if msg[0] == "help_on":
